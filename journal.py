be2bf73c1ddc98ec75591d5600dfeedefcb55467
Matt Gaughan <mattgaugs@gmail.com>
2023-11-07 19:04:02 -0500

    journal

---
+++ journal.py
@@ -0,0 +1,11 @@
+n = str(input("Where do you want to travel? "))
+
+if n == 'New York':
+    print(f"Enjoy your stay in {n}!")
+elif n == 'Las Vegas':
+    print(f"Enjoy your stay in {n}!")
+elif n == 'St. Louis':
+    print(f"Enjoy your stay in {n}!")
+else:
+    print(f"Oops... that destination is not available. Sorry")
+    
