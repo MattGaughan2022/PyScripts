13b2b6fb11ffc82d6dc477acb2f5dd760445cc6d
Matt Gaughan <mattgaugs@gmail.com>
2024-02-12 08:30:08 -0500

    sqlalc app

--- task 1.7/recipe_app.py
+++ task 1.7/recipe_app.py
@@ -22,7 +22,9 @@ class Recipe(Base):
     difficulty = Column(String(50))
 
     def __repr__(self):
-        return "<Recipe ID: " + str(self.id) + "-" + self.name + ">"
+        return "<" + str(self.id) + ", " + self.name + ", " + self.difficulty + ">"
+    def __str__(self):
+        return "ID: " + str(self.id) + " | Name: " + self.name + " | Ingredients: " + self.ingredients + " | cooking_time: " + str(self.cooking_time) + " | Difficulty: " + self.difficulty
     
 
 Base.metadata.create_all(engine)
@@ -42,7 +44,12 @@ def calculate_difficulty(time, ingredients):
 def create_recipe():
     print("===== CREATE RECIPE =====")
     r_name = input("Name of recipe: ")
-    r_cooking_time = int(input("Cooking time for recipe: "))
+    r_cooking_time = input("Cooking time for recipe: ")
+    try:
+        r_cooking_time = int(r_cooking_time)
+    except:
+        input("cooking time that is entered is not an integer value. Returning...")
+        return None
     r_ingredients = []
     sql_ingredients = ""
     m = int(input("How many ingredients are in " + r_name + "? "))
@@ -71,7 +78,7 @@ def view_recipes():
     result = session.query(Recipe).all()
     i = 0
     for row in result:
-        print("ID:", row.id, " |  RECIPE: " + row.name, " |  INGREDIENTS: "+  row.ingredients, " |  COOKING TIME:",  row.cooking_time, " |  DIFFICULTY: "+  row.difficulty)
+        print(row)
     input("Press enter to return...")
 
 def search_ingredients():
@@ -96,20 +103,37 @@ def search_ingredients():
 
 def update_recipe():
     result = session.query(Recipe).all()
-    i = 0
+    i = []
     try:
         result[0]
     except:
         input("Empty table. Please choose to create a recipe. Press enter to return...")
-        return True
+        return None
     for row in result:
-        if(row.id > i):
-            i = row.id
-        print("ID:", row.id, " |  RECIPE: " + row.name, " |  INGREDIENTS: "+  row.ingredients + " | COOKING_TIME: " + str(row.cooking_time))
+        i.append(row.id)
+        print(row)
     id_update = input("Select the ID of a recipe you wish to UPDATE: ")
-    column_update = input("Which category/column did you want to update? (name, ingredients, cooking_time)")
+    try: 
+        id_update = int(id_update)
+        if(id_update in i):
+            next
+        else:
+            input("Bad input. Back to main loop...")
+            return 1
+    except:
+        input("Bad input. Back to main loop...")
+        return 1
+
+    column_update = input("Which category/column did you want to update? (name, ingredients, cooking_time): ")
     if(column_update == ('name' or 'Name')):
-        change = input("What would you like to change [" + row.name + "] to? ")
+        change = input("What would you like to change the name of [" + row.name + "] to? ")
+        if(len(change) > 0 and len(change) < 50):
+            session.query(Recipe).filter(Recipe.name == row.name).update({Recipe.name: change})
+            session.commit
+        else:
+            print("Please enter a name length less than 50")
+            update_recipe()
+
     elif(column_update == ('ingredients' or 'Ingredients')):
         r_ingredients = []
         sql_ingredients = ""
@@ -122,61 +146,56 @@ def update_recipe():
             else:
                 sql_ingredients += (p + ",")
         r_diff = calculate_difficulty(row.cooking_time, r_ingredients)
-
+        session.query(Recipe).filter(Recipe.name == row.name).update({Recipe.ingredients: sql_ingredients, Recipe.difficulty: r_diff})
+        session.commit
         
-    elif(column_update == ('cooking_time' or 'cooking time')):
-        change = int(input("What would you like to change [" + row.name + "]'s cooking time to? "))
-        r_diff = calculate_difficulty(change, r_ingredients)
-
-    try: 
-        id_update = int(id_update)
-        if(id_update > 0 and id_update <= i):
-            try:
-                to_update = session.query(Recipe).filter(Recipe.id == id_update).one()
-                session.commit()
-                return True
-            except:
-                input("Error. Potentially an empty ID. Please try again with an existing ID...")
-                return True
-        elif(id_update <= 0 or id_update > i):
-            input("Bad input. Back to main loop")
-            return True
-    except:
-        input("Bad input. Back to main loop")
-        return True
+    elif(column_update == ('cooking_time')):
+        change = input("What would you like to change [" + row.name + "]'s cooking time to? ")
+        try:
+            r_cooking_time = int(r_cooking_time)
+        except:
+            print("cooking time that is entered is not an integer value.")
+            return 1
+        r_diff = calculate_difficulty(change, row.ingredients)
+        session.query(Recipe).filter(Recipe.name == row.name).update({Recipe.cooking_time: change, Recipe.difficulty: r_diff})
+        session.commit
 
 
 
 def delete_recipe():
     result = session.query(Recipe).all()
-    i = 0
+    i = []
     try:
         result[0]
     except:
         input("Empty table. Please choose to create a recipe. Press enter to return...")
-        return True
+        return None
     for row in result:
-        if(row.id > i):
-            i = row.id
+        i.append(row.id)
+        print(row)
         print("ID:", row.id, " |  RECIPE: " + row.name, " |  INGREDIENTS: "+  row.ingredients)
     id_delete = input("Select the ID of a recipe you wish to delete: ")
     try: 
-        id_delete = int(id_delete)
-        if(id_delete > 0 and id_delete <= i):
+        if(id_delete in i):
             try:
                 to_delete = session.query(Recipe).filter(Recipe.id == id_delete).one()
-                session.delete(to_delete)
-                session.commit()
-                return True
+                check = input("Are you sure you want to delete " + to_delete.name + "? (Y or N)")
+                if(check == "Y" or "y"):
+                    session.delete(to_delete)
+                    session.commit()
+                    return None
+                else:
+                    input("Not deleting. Returning...")
+                    return None
             except:
                 print("Error. Potentially an empty ID. Please try again with an existing ID...")
-                return True
-        elif(id_delete <= 0 or id_delete > i):
+                return None
+        else:
             print("Bad input. Back to main loop")
-            return True
+            return None
     except:
         print("Bad input. Back to main loop")
-        return True
+        return None
 
 
 def main_loop():
@@ -184,32 +203,33 @@ def main_loop():
     if(len(recipes_list) > 0):
         print(recipes_list)
     choice = None
-    print("Main Menu")
-    print("============================")
-    print("Pick a choice:")
-    print("     1. Create New Recipe")
-    print("     2. View all recipes")
-    print("     3. Search for a recipe by ingredient")
-    print("     4. Update an existing recipe")
-    print("     5. Delete a recipe")
-    print("     6. Quit the program...")
-    choice = input("Your choice: ")
-    if choice == '1':
-        create_recipe()
-    elif choice == '2':
-        view_recipes()
-    elif choice == '3':
-        search_ingredients()
-    elif choice == '4':
-        update_recipe()
-    elif choice == '5':
-        delete_recipe()
-    else:
-        print("Exiting...")
-        return False
+    while choice == None or 1 or 2 or 3 or 4 or 5:
+        print("Main Menu")
+        print("============================")
+        print("Pick a choice:")
+        print("     1. Create New Recipe")
+        print("     2. View all recipes")
+        print("     3. Search for a recipe by ingredient")
+        print("     4. Update an existing recipe")
+        print("     5. Delete a recipe")
+        print("     6. Quit the program...")
+        choice = input("Your choice: ")
+        if choice == '1':
+            create_recipe()
+        elif choice == '2':
+            view_recipes()
+        elif choice == '3':
+            search_ingredients()
+        elif choice == '4':
+            update_recipe()
+        elif choice == '5':
+            delete_recipe()
+        else:
+            print("Exiting...")
+            return False
 
 
 
 
-while True:
-    main_loop()
+
+main_loop()
